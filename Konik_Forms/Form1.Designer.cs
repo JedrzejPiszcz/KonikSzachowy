53f736dae79a0c5cc9907147f644abb05e65a7d7
JedrzejPiszcz <divineet@gmail.com>
2018-01-12 19:24:06 +0100

    Initial Commit

---
+++ Konik_Forms/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Konik_Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.MoveKnight = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(160, 267);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(47, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(160, 311);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(47, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(160, 355);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(47, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 270);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Size";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 314);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Start point x";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(227, 318);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Start point y";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(310, 311);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(47, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(77, 362);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "End point x";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(227, 362);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "End point y";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(310, 362);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(47, 20);
+            this.textBox5.TabIndex = 10;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(310, 260);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(53, 27);
+            this.StartButton.TabIndex = 11;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // MoveKnight
+            // 
+            this.MoveKnight.Location = new System.Drawing.Point(385, 309);
+            this.MoveKnight.Name = "MoveKnight";
+            this.MoveKnight.Size = new System.Drawing.Size(87, 22);
+            this.MoveKnight.TabIndex = 12;
+            this.MoveKnight.Text = "Move Knight";
+            this.MoveKnight.UseVisualStyleBackColor = true;
+            this.MoveKnight.Click += new System.EventHandler(this.MoveKnight_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 406);
+            this.Controls.Add(this.MoveKnight);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button StartButton;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button MoveKnight;
+    }
+}
+
---
+++ Konik_Forms/Form1.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Konik_Forms
+{
+    public partial class Form1 : Form
+    {
+
+        int boardSize = 1;
+        int _boardX = 10, _boardY = 10;
+        int _boardWidth = 240;
+        int _boardHeight = 240;
+        Brush _borderColor = Brushes.Black;
+        Brush _startColor = Brushes.Green;
+        Brush _endColor = Brushes.Red;
+        Brush _visitedField = Brushes.Gold;
+        Brush _knightColor = Brushes.Orange;
+        Brush[] _boardColor = { Brushes.White, Brushes.Black };
+
+        board chessboard;
+        List<knightMove> knightMoves = new List<knightMove>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) //Size
+        {
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            
+
+            if (textBox1.Text != "") {
+                boardSize = Int32.Parse(textBox1.Text);
+            };
+
+            boardPoint startPoint = new boardPoint(Int32.Parse(textBox2.Text), Int32.Parse(textBox4.Text));
+            boardPoint endPoint = new boardPoint(Int32.Parse(textBox3.Text), Int32.Parse(textBox5.Text));
+
+            
+            chessboard = new board(boardSize, startPoint, endPoint);
+            chessboard.startPoint = startPoint;
+            chessboard.endPoint = endPoint;
+
+            this.Paint += Form1_Paint;
+            this.Invalidate();
+
+        }
+
+        void drawChessBoard(Graphics g)
+        {
+            //draw squares
+            int boardSize = chessboard.boardSize;
+
+            int spacingX = _boardWidth / boardSize;
+            int spacingY = _boardHeight / boardSize;
+            for (int col = 0; col < boardSize; col++)
+            {
+                for (int row = 0; row < boardSize; row++)
+                {
+                
+                    g.FillRectangle(_boardColor[(col + row) % 2], _boardX + col * spacingX, _boardY + row * spacingY, spacingX, spacingY);
+                }
+            }
+
+            //draw visited points
+            var visitedPoints = chessboard.getVisitedPoints();
+            foreach (var point in visitedPoints)
+            {
+                g.FillRectangle(_visitedField, _boardX + point.coordinateX * spacingX, _boardY + point.coordinateY * spacingY, spacingX, spacingY);
+            }
+
+            //draw start and end points
+            g.FillRectangle(_endColor, _boardX + chessboard.endPoint.coordinateX * spacingX, _boardY + chessboard.endPoint.coordinateY * spacingY, spacingX, spacingY);
+            g.FillRectangle(_startColor, _boardX + chessboard.startPoint.coordinateX * spacingX, _boardY + chessboard.startPoint.coordinateY * spacingY, spacingX, spacingY);
+
+            //draw current knight position
+            g.FillRectangle(_knightColor, _boardX + chessboard.getKnightPoint().coordinateX * spacingX, _boardY + chessboard.getKnightPoint().coordinateY * spacingY, spacingX, spacingY);
+
+
+            //draw border
+            g.DrawRectangle(new Pen(_borderColor, 1), new Rectangle(_boardX, _boardY, _boardWidth, _boardHeight));
+        }
+
+        private void MoveKnight_Click(object sender, EventArgs e)
+        {
+            //create the first postion with no move
+            knightMoves.Add(new knightMove(1));
+            knightMoves.LastOrDefault().currentPoint = chessboard.startPoint;
+
+
+            while (!chessboard.checkSuccessCondition())
+            {
+                //set wrong move to ignore
+                chessboard.wrongMoves = knightMoves.ElementAt(knightMoves.Count - 1).wrongMoves;
+                if (chessboard.getAvilablePoints().Count() > 0)
+                {
+                    //add new move
+                    knightMoves.Add(new knightMove(knightMoves.Max(p => p.moveId) + 1));
+                    //if any moves exists add current position as previous position 
+                    knightMoves.LastOrDefault().previousPoint = chessboard.getKnightPoint();
+
+                    //really move the knight 
+                    
+                    chessboard.moveKnight();
+
+                    //get new position as current position
+                    knightMoves.LastOrDefault().currentPoint = chessboard.getKnightPoint();
+                }
+                else
+                {
+
+                    //save current point
+                    var currentPoint = chessboard.getKnightPoint();
+
+                    //add current point to the wrong moves list
+                    knightMoves.LastOrDefault().wrongMoves.Add(currentPoint);
+
+                    //move the knight back
+                    chessboard.undoKnight(knightMoves.LastOrDefault().previousPoint , knightMoves.LastOrDefault().currentPoint);
+
+                    //remove the last (wrong) move from the list
+                    knightMoves.RemoveAt(knightMoves.Count - 1);
+
+                    //add current point to the wrong moves list
+                    knightMoves.LastOrDefault().wrongMoves.Add(currentPoint);
+
+
+                }
+               // System.Threading.Thread.Sleep(1);
+                this.Invalidate();
+                Application.DoEvents();
+            }
+        }
+
+        //Form1 Paint method calls DrawChessBoard
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            drawChessBoard(e.Graphics);
+        }
+    }
+    public class boardPoint
+    {
+        public int coordinateX { get; }
+        public int coordinateY { get; }
+        public bool visited { get; set; }
+        public bool knightVisiting { get; set; }
+
+        public boardPoint(int x, int y)
+        {
+            this.coordinateX = x;
+            this.coordinateY = y;
+        }
+
+    }
+
+    public class board
+    {
+        private int _boardSize = 1;
+        public int boardSize
+        {
+            get
+            {
+                return _boardSize;
+            }
+            set
+            {
+                if (boardSize <= 0)
+                {
+                    throw new Exception("Rozmiar tablicy musi być większy od 0!");
+                }
+                else
+                {
+                    _boardSize = value;
+                }
+            }
+        }
+
+        private boardPoint _startPoint;
+        public boardPoint startPoint
+        {
+            get
+            {
+                return _startPoint;
+            }
+            set
+            {
+                if (value.coordinateX > boardSize || value.coordinateY > boardSize)
+                {
+                    throw new Exception("Punkt początkowy musi znajdować się na tablicy!");
+                }
+                else
+                {
+                    _startPoint = value;
+                }
+            }
+        }
+
+        private boardPoint _endPoint;
+        public boardPoint endPoint
+        {
+            get
+            {
+                return _endPoint;
+            }
+            set
+            {
+                if (value.coordinateX > boardSize || value.coordinateY > boardSize)
+                {
+                    throw new Exception("Punkt końcowy musi znajdować się na tablicy!");
+                }
+                else
+                {
+                    _endPoint = value;
+                }
+            }
+        }
+
+        public List<boardPoint> wrongMoves { get; set; }
+
+        private List<boardPoint> boardArray = new List<boardPoint>();
+
+        
+        public board(int boardSize, boardPoint startPoint, boardPoint endPoint)
+        {
+            this.boardSize = boardSize;
+            for (int i = 0; i < boardSize; i++)
+            {
+                for (int j = 0; j < boardSize; j++)
+                {
+                    if (startPoint.coordinateX == i && startPoint.coordinateY == j)
+                    {
+                        startPoint.knightVisiting = true;
+                        boardArray.Add(startPoint);
+
+                    }
+                    else if (endPoint.coordinateX == i && endPoint.coordinateY == j)
+                    {
+                        boardArray.Add(endPoint);
+                    }
+                    else
+                    {
+                        boardArray.Add(new boardPoint(i, j));
+                    }
+                }
+            }
+        }
+
+        public List<boardPoint> getVisitablePoints()
+        {
+            return boardArray.Where(p =>       (p.coordinateX - 2 == getKnightPoint().coordinateX && p.coordinateY - 1 == getKnightPoint().coordinateY) ||
+                                               (p.coordinateX - 2 == getKnightPoint().coordinateX && p.coordinateY + 1 == getKnightPoint().coordinateY) ||
+                                               (p.coordinateX + 2 == getKnightPoint().coordinateX && p.coordinateY - 1 == getKnightPoint().coordinateY) ||
+                                               (p.coordinateX + 2 == getKnightPoint().coordinateX && p.coordinateY + 1 == getKnightPoint().coordinateY) ||
+                                               (p.coordinateX - 1 == getKnightPoint().coordinateX && p.coordinateY - 2 == getKnightPoint().coordinateY) ||
+                                               (p.coordinateX - 1 == getKnightPoint().coordinateX && p.coordinateY + 2 == getKnightPoint().coordinateY) ||
+                                               (p.coordinateX + 1 == getKnightPoint().coordinateX && p.coordinateY - 2 == getKnightPoint().coordinateY) ||
+                                               (p.coordinateX + 1 == getKnightPoint().coordinateX && p.coordinateY + 2 == getKnightPoint().coordinateY)).ToList();
+        }
+
+        public List<boardPoint> getAvilablePoints()
+        {
+            return getVisitablePoints().Where(p => p.visited == false && 
+                                                  !(p.coordinateX == endPoint.coordinateX && p.coordinateY == endPoint.coordinateY)).ToList().Except(wrongMoves).ToList();
+        }
+
+        public bool checkSuccessCondition()
+        {
+            if(boardArray.Where(p => p.visited == true).Count() == (Math.Pow(boardSize, 2)-2))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void moveKnight()
+        {
+            //randomly selects new jump for the knight
+            Random rnd = new Random();
+
+
+            var avilablePoints = getAvilablePoints();
+            var knightPoint = getKnightPoint();
+            int nextJump = rnd.Next(avilablePoints.Count());
+
+            //moves the knight
+            knightPoint.visited = true;
+            knightPoint.knightVisiting = false;
+            avilablePoints.ElementAt(nextJump).knightVisiting = true;
+            
+        }
+
+        public void undoKnight(boardPoint previousPoint, boardPoint currentPoint)
+        {
+            currentPoint.knightVisiting = false;
+            previousPoint.visited = false;
+            previousPoint.knightVisiting = true;
+
+        }
+
+        public boardPoint getKnightPoint()
+        {
+            return boardArray.Where(p => p.knightVisiting == true).FirstOrDefault();
+        }
+
+        public List<boardPoint> getVisitedPoints()
+        {
+            return boardArray.Where(p => p.visited == true).ToList();
+        }
+
+
+    }
+
+    public class knightMove
+    {
+        public int moveId { get; set; }
+        public boardPoint previousPoint { get; set; }
+        public boardPoint currentPoint { get; set; }
+        public List<boardPoint> wrongMoves = new List<boardPoint>();
+
+        public knightMove(int moveId)
+        {
+            this.moveId = moveId;
+        }
+
+
+    }
+}
